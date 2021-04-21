cbe1202b29922f89ecb84f0b8710a45ccbb59ac2
Yassien Najem <yasen2441@gmail.com>
2021-04-21 16:30:28 +0000

    creat pagination and like

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,23 @@
 
 import React, { Component } from "react";
+import Like from './common/like';
+import Pagination from './common/pagination';
 import { getMovies } from "../services/fakeMovieService";
-
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component{
   state = {
-    movies:getMovies()
+    movies:getMovies(),
+    pageSize: 4,
+    currentPage:1
+  };
+
+  handleLiked = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies }); 
   };
 
   handleDelete = movie =>{
@@ -13,11 +25,21 @@ class Movies extends Component{
     this.setState({movies});
   };
 
+  handlePageChange = page => {
+    this.setState({ currentPage : page });
+  }
+
+
   render(){
-    const { length: count} = this.state.movies;
+    const { length: count } = this.state.movies;
+    const {pageSize , currentPage , movies:allMovies} = this.state;
+
+    
 
     if(count === 0)
       return <p className="m-3">there is no movies in database</p>;
+
+    const movies = paginate(allMovies , currentPage ,pageSize)
 
     return (
       <React.Fragment>
@@ -30,21 +52,32 @@ class Movies extends Component{
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie =>(
+            {movies.map(movie =>(
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+
+                <td>
+                  <Like liked={movie.liked} 
+                  onClick={() => this.handleLiked(movie)}/>
+                </td>
                 <td><button onClick={()=> this.handleDelete(movie)}
                 className="btn btn-danger btn-sm">Delate</button></td>
               </tr>
               ))}
           </tbody>
         </table>
+        <Pagination 
+        itemsCount={count} 
+        pageSize={pageSize} 
+        onPageChange={this.handlePageChange}
+        currentPage={currentPage}/>
       </React.Fragment>
     );
   };
--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -7,7 +7,7 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 6,
     dailyRentalRate: 2.5,
-    publishDate: "2018-01-03T19:04:28.809Z"
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
@@ -15,6 +15,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
     dailyRentalRate: 2.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
@@ -22,6 +24,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 8,
     dailyRentalRate: 3.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
@@ -29,6 +33,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
     dailyRentalRate: 3.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
@@ -36,6 +42,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
     dailyRentalRate: 3.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
@@ -43,6 +51,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
     dailyRentalRate: 3.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
@@ -50,6 +60,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 7,
     dailyRentalRate: 4.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
@@ -57,6 +69,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 4,
     dailyRentalRate: 3.5
+    ,
+    liked: true
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821",
@@ -64,6 +78,8 @@ const movies = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 7,
     dailyRentalRate: 3.5
+    ,
+    liked: false
   }
 ];
 
