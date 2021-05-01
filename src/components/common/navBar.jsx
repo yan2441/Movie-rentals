be0b535491e11e6ec561a7e1127162d93e7a52d8
Yassien Najem <yasen2441@gmail.com>
2021-05-01 04:29:14 +0000

    small fix

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -5,7 +5,7 @@ import { NavLink ,Link} from 'react-router-dom';
 const NavBar = () => {
   return ( <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <Link className="navbar-brand" to="/">
-    Vidly
+    MOVIE4U
   </Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
