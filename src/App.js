d23e6d594d5a4ca0ea6a54ed343006eb04dd77ea
Yassien Najem <yasen2441@gmail.com>
2021-05-30 23:37:34 +0100

    implament logout feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route , Redirect ,Switch} from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import Movies from './components/movies';
 import Customers from './components/customers';
@@ -7,6 +7,7 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/common/navBar';
 import MovieForm from './components/movieForm';
+import Logout from './components/logout';
 import LoginForm from './components/common/loginForm';
 import RegisterForm from './components/common/registerForm';
 import "./App.css";
@@ -16,38 +17,37 @@ import "./App.css";
 
 
 class App extends Component {
-  state ={}
+  state = {}
 
-  componentDidMount(){
+  componentDidMount() {
     try {
       const jwt = localStorage.getItem('token')
       const user = jwtDecode(jwt)
-      this.setState({user})
-      
+      this.setState({ user })
     } catch (ex) {
-      
     }
   }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <React.Fragment>
-      <NavBar user={this.state.user}/>
-      <main className="container">
-        <Switch>
-        <Route path="/movies/:id" component={MovieForm}></Route>
-        <Route path="/login" component={LoginForm}></Route>
-        <Route path="/register" component={RegisterForm}></Route>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies"/>
-        <Redirect to="/not-found"/>
-        </Switch>
-      </main>
+        <NavBar user={this.state.user} />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
-     );
+    );
   }
 }
 
