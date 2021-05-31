24d19e403c4014e182d6aceb03e1a5a4cd936be6
Yassien Najem <yasen2441@gmail.com>
2021-05-31 19:39:43 +0100

    make delete for admin only

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,32 +1,53 @@
-import React ,{Component}from 'react';
+import React, { Component } from 'react';
 import Table from "./common/table";
 import Like from './common/like';
 import { Link } from 'react-router-dom';
+import { getCurrentUser } from '../services/authService';
+
 
 
 
 class MoviesTable extends Component {
   columns = [
-    { path: 'title' , label: 'Title' , content : movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>},
-    { path: 'genre.name' , label: 'Genre'},
-    { path: 'numberInStock' , label: 'Stock'},
-    { path: 'dailyRentalRate' , label: 'Rate'},
-    { key: 'like', content : movie => (<Like liked={movie.liked} onClick={() => this.props.onLike(movie)}/>)},
-    { key: 'delete', content: movie => <button onClick={()=> this.props.onDelete(movie)} className="btn btn-danger btn-sm"> Delate</button>},
+    { path: 'title', label: 'Title', content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
+    { path: 'genre.name', label: 'Genre' },
+    { path: 'numberInStock', label: 'Stock' },
+    { path: 'dailyRentalRate', label: 'Rate' },
+    { key: 'like', content: movie => (<Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />) },
+
   ];
-  
 
-  render() { 
-    const { onSort,sortColumn ,movies}= this.props;
+  deleteColumn = {
+    key: 'delete',
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delate
+      </button>
+    )
+  };
 
-    return ( 
+  constructor() {
+    super()
+    const user = getCurrentUser();
+    if (user && user.isAdmin)
+      this.columns.push(this.deleteColumn)
+  }
+
+
+  render() {
+    const { onSort, sortColumn, movies } = this.props;
+
+    return (
       <Table
-      columns={this.columns}
-      sortColumn={sortColumn}
-      onSort={onSort}
-      data={movies}
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        data={movies}
       />
-     );
+    );
   }
 }
 
